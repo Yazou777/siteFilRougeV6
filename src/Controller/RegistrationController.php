f1ad51e0e90a2da1891aa0acfe0b50af9467cc21
Yazou777 <wilga.kevin@sfr.fr>
2023-12-01 11:54:52 +0100

    mailer et mailhog

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -12,9 +12,18 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class RegistrationController extends AbstractController
 {
+    private $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, LoginFormAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
@@ -34,7 +43,16 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
+            //envoi un email de confirmation
+            $email = (new Email())
+                ->from('hello@example.com')
+                ->to($user->getEmail())
+                ->subject('Confirmation de votre inscription')
+                ->text('Bienvenue sur notre site !')
+                ->html('<p>Bienvenue sur notre site !</p>');
 
+            $this->mailer->send($email);  
+//dd($user->getEmail());
             return $userAuthenticator->authenticateUser(
                 $user,
                 $authenticator,
